80417a1a301d02da33e24f871c7afc194717c2e2
AFF40 <alejandro.fernando.flores99@gmail.com>
2024-05-24 00:51:37 -0400

    Removed old submodule web2

--- web3/controlador/cont_rest.php
+++
@@ -1,6 +0,0 @@
-<?php
- include "../modelo/Restaurante.php";
- $restaurante = new Restaurante_completo();
- $restaurante->mostrarEnJSON();
-
-?>
--- web3/modelo/conexion/configDb.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Definicion de Constantes
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '1234');
-define('DATABASE', 'androidbd');
